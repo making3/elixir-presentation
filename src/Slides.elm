48226981512964893598ebdf459d115d19017849
Matthew King <mattking73@gmail.com>
2018-03-20 16:31:18 -0700

    Update to include presentation (not complete)

--- src/Slides.elm
+++ src/Slides.elm
@@ -8,12 +8,78 @@ main =
         slidesDefaultOptions
         [ md
             """
-        # A markdown slide
-        _stuff follows..._
+        # Elixir
+        _Fun & reliable functional programming_
         """
         , mdFragments
-            [ "Another slide with three fragments"
-            , "This appears later"
-            , "And this even later"
+            [ "# My History"
+            , "* .NET (VB -> C#)"
+            , "* NodeJS"
+            , "* Elixir"
             ]
+        , md
+            """
+            # Agenda?
+            * What is Elixir?
+            * Why Elixir
+            * Why Not Elixir?
+            * Who's using Elixir? Erlang?
+            * Examples
+            """
+        , mdFragments
+            [ "# What is Elixir?"
+            , "* Dynamic, functional language for building scalable and maintainable applications"
+            , "* Runs on the Erlang virtual machine (BEAM)"
+            , "* Anything written in Erlang can be ran in Elixir"
+            ]
+        , md
+            """
+        # Who uses Elixir?
+          * Discord
+          * Pinterest
+          * HelloSign
+        """
+        , md
+            """
+        # Example Elixir
+
+        TODO: Fill in sample Elixir code
+        ```elixir
+        ```
+        """
+        , mdFragments
+            [ "# Erlang"
+            , "* Designed for high uptime"
+            , "* Erlang first appeared in 1986"
+            , "* If Java is 'write once, run anywhere', then Erlang is 'write once, run forever'. - [Joe Armstring](https://www.youtube.com/watch?v=u41GEwIq2mE&t=3m59s)"
+            ]
+        , md
+            """
+        # Example Erlang
+
+        TODO: Fill in sample Erlang code
+        ```erlang
+        ```
+        """
+        , md
+            """
+        # Who uses Erlang?
+          * Facebook
+          * WhatsApp
+          * RabbitMQ
+          * Amazon SimpleDB
+        """
+        , mdFragments
+            [ "# Fault Tolerance" ]
+        , mdFragments
+            [ "# Hot Code Reloading" ]
+        , mdFragments
+            [ "# Other Features"
+            , "* Functional Programming"
+            , "* Extensibility"
+            ]
+        , md
+            """
+        Demo Time!
+        """
         ]
