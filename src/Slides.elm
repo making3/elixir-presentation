1e901faaefa765ad5a371ecff42a99b25cfa0c3d
Matthew King <mattking73@gmail.com>
2018-03-20 15:23:52 -0700

    Initialize initial repo with elm slides functionality

---
+++ src/Slides.elm
@@ -0,0 +1,19 @@
+module Main exposing (..)
+
+import Slides exposing (..)
+
+
+main =
+    Slides.app
+        slidesDefaultOptions
+        [ md
+            """
+        # A markdown slide
+        _stuff follows..._
+        """
+        , mdFragments
+            [ "Another slide with three fragments"
+            , "This appears later"
+            , "And this even later"
+            ]
+        ]
