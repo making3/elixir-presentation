89aaf6f902b920423e0eebd57379ee5ffe6f6fa3
Matthew King <mattking73@gmail.com>
2018-03-21 11:21:37 -0700

    Add loop demo code

--- chat/lib/chat.ex
+++ chat/lib/chat.ex
@@ -15,4 +15,21 @@ defmodule Chat do
   def hello do
     :world
   end
+
+  def start do
+    spawn(fn -> loop(0) end)
+  end
+
+  def loop(uptime) do
+    receive do
+      :stop ->
+        IO.puts("Shutting down")
+        exit(:normal)
+    after
+      2000 ->
+        IO.puts("#{uptime} years of bgu-free uptime on #{node()}.")
+    end
+
+    Chat.loop(uptime + 1)
+  end
 end
